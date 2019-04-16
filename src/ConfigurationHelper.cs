9467d7bfe28baae8dba61f687e16ca2fd0c7bfda
Oli Newsham <oli.newsham@gmail.com>
2019-04-16 22:53:56 +0100

    Initial simple version showing how to read and write encrypted values

---
+++ src/ConfigurationHelper.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Configuration;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace AppConfig
+{
+    public class ConfigurationHelper
+    {
+        private readonly Configuration _config;
+
+        public ConfigurationHelper()
+        {
+            _config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+        }
+
+        public void WriteEncryptedValue(string key, string value)
+        {
+            WriteValue(key, value, true);
+        }
+
+        public void WriteClearTextValue(string key, string value)
+        {
+            WriteValue(key, value, false);
+        }
+
+        public string ReadEncryptedValue(string key, string defaultValue = null)
+        {
+            return ReadValue(key, defaultValue, true);
+        }
+
+        public string ReadClearTextValue(string key, string defaultValue = null)
+        {
+            return ReadValue(key, defaultValue, false);
+        }
+
+        private void WriteValue(string key, string value, bool encryptValue)
+        {
+            if (_config.AppSettings.Settings.AllKeys.Contains(key))
+            {
+                _config.AppSettings.Settings.Remove(key);
+            }
+
+            _config.AppSettings.Settings.Add(key, encryptValue ? Encrypt(value) : value);
+
+            _config.Save();
+        }
+
+        private string ReadValue(string key, string defaultValue, bool decryptValue)
+        {
+            if (!_config.AppSettings.Settings.AllKeys.Contains(key))
+            {
+                return defaultValue;
+            }
+
+            var decryptedValue = _config.AppSettings.Settings[key].Value;
+
+            return decryptValue ? Decrypt(decryptedValue) : decryptedValue;
+        }
+
+        private static string Encrypt(string unencryptedValue)
+        {
+            var unencryptedInputBytes = Encoding.Unicode.GetBytes(unencryptedValue);
+            var encryptedValue = ProtectedData.Protect(unencryptedInputBytes, null, DataProtectionScope.CurrentUser);
+            var base64EncryptedValue = Convert.ToBase64String(encryptedValue);
+
+            return base64EncryptedValue;
+        }
+
+        private static string Decrypt(string encryptedBase64Value)
+        {
+            var encryptedInputBytes = Convert.FromBase64String(encryptedBase64Value);
+            var unencryptedValueBytes = ProtectedData.Unprotect(encryptedInputBytes, null, DataProtectionScope.CurrentUser);
+            var unencryptedValue = Encoding.Unicode.GetString(unencryptedValueBytes);
+
+            return unencryptedValue;
+        }
+    }
+}
---
+++ src/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AppConfig
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            // 1. Add a reference to System.Configuration and System.Security
+
+            var configuration = new ConfigurationHelper();
+
+            configuration.WriteEncryptedValue("AlpacaFluffinessLevel", "Dangerously High");
+            configuration.WriteClearTextValue("NumberOfAlpacas", "9000");
+
+            var alpacaFluffinessLevel = configuration.ReadEncryptedValue("AlpacaFluffinessLevel");
+            var numberOfAlpacas = configuration.ReadClearTextValue("NumberOfAlpacas");
+
+            Console.WriteLine($"Decrypted Alpaca Fluffiness Level is '{alpacaFluffinessLevel}'");
+            Console.WriteLine($"Clear Text Number of Alpacas is '{numberOfAlpacas}'");
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadLine();
+        }
+    }
+}
